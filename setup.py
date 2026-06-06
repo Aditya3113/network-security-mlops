da4c5758e87b53b268c0dc828f1ae73e6763c7bb
Aditya Pandey <aditya3113pandey@gmail.com>
2026-06-06 23:51:24 +0530

    build: implement core project structure and base configurations

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="network_security",
+    version="0.0.1",
+    author="Aditya",
+    author_email="aditya3113pandey@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
