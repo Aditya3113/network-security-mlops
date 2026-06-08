88980abea32aea777b4cc6d4942725cc82be2e5c
Aditya Pandey <aditya3113pandey@gmail.com>
2026-06-08 22:22:53 +0530

    feat: implement data validation component and execute in main pipeline

--- main.py
+++ main.py
@@ -2,26 +2,31 @@ import sys
 import os
 from dotenv import load_dotenv
 
+load_dotenv()
+
 from network_security.components.data_ingestion import DataIngestion
-from network_security.entity.config_entity import TrainingPipelineConfig, DataIngestionConfig
+from network_security.components.data_validation import DataValidation
+from network_security.entity.config_entity import DataIngestionConfig, DataValidationConfig, TrainingPipelineConfig
 from network_security.exception import NetworkSecurityException
 from network_security.logging import logger
-
-load_dotenv()
 
 if __name__ == "__main__":
     try:
         training_pipeline_config = TrainingPipelineConfig()
+       
+        logger.info("Starting Data Ingestion Phase...")
         data_ingestion_config = DataIngestionConfig(training_pipeline_config)
-        
-        logger.info("Initializing Data Ingestion Component")
         data_ingestion = DataIngestion(data_ingestion_config)
-        
         data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
-        
-        print("Data Ingestion Completed Successfully!")
-        print(f"Train Dataset Saved At: {data_ingestion_artifact.train_file_path}")
-        print(f"Test Dataset Saved At: {data_ingestion_artifact.test_file_path}")
+        logger.info("Data Ingestion Completed Successfully!")
+        print(data_ingestion_artifact)
+
+        logger.info("Starting Data Validation Phase...")
+        data_validation_config = DataValidationConfig(training_pipeline_config)
+        data_validation = DataValidation(data_ingestion_artifact, data_validation_config)
+        data_validation_artifact = data_validation.initiate_data_validation()
+        logger.info("Data Validation Completed Successfully!")
+        print(data_validation_artifact)
         
     except Exception as e:
         raise NetworkSecurityException(e, sys)
---
+++ network_security/utils/main_utils.py
@@ -0,0 +1,22 @@
+import os
+import sys
+import yaml
+from network_security.exception import NetworkSecurityException
+from network_security.logging import logger
+
+def read_yaml_file(file_path: str) -> dict:
+    try:
+        with open(file_path, "rb") as yaml_file:
+            return yaml.safe_load(yaml_file)
+    except Exception as e:
+        raise NetworkSecurityException(e, sys)
+
+def write_yaml_file(file_path: str, content: object, replace: bool = False) -> None:
+    try:
+        if replace and os.path.exists(file_path):
+            os.remove(file_path)
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        with open(file_path, "w") as file:
+            yaml.dump(content, file)
+    except Exception as e:
+        raise NetworkSecurityException(e, sys)
