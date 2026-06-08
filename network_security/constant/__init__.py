43ff13d50e5985a99394f5303badd3f4a41794f6
Aditya Pandey <aditya3113pandey@gmail.com>
2026-06-08 21:28:23 +0530

    feat: define configuration and artifacts for data validation

--- network_security/constant/__init__.py
+++ network_security/constant/__init__.py
@@ -12,3 +12,10 @@ DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO: float = 0.2
 #Database
 DATABASE_NAME: str = "NetworkData"
 COLLECTION_NAME: str = "NetworkStore"
+
+#Validation
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+DATA_VALIDATION_VALID_DIR: str = "validated"
+DATA_VALIDATION_INVALID_DIR: str = "invalid"
+DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report"
+DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
--- network_security/entity/config_entity.py
+++ network_security/entity/config_entity.py
@@ -23,3 +23,20 @@ class DataIngestionConfig:
             self.data_ingestion_dir, DATA_INGESTION_INGESTED_DIR, "test.csv"
         )
         self.train_test_split_ratio: float = DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO
+
+class DataValidationConfig:
+    def __init__(self, training_pipeline_config: TrainingPipelineConfig):
+        self.data_validation_dir: str = os.path.join(
+            training_pipeline_config.artifact_dir, DATA_VALIDATION_DIR_NAME
+        )
+        self.valid_data_dir: str = os.path.join(self.data_validation_dir, DATA_VALIDATION_VALID_DIR)
+        self.invalid_data_dir: str = os.path.join(self.data_validation_dir, DATA_VALIDATION_INVALID_DIR)
+        self.valid_train_file_path: str = os.path.join(self.valid_data_dir, "train.csv")
+        self.valid_test_file_path: str = os.path.join(self.valid_data_dir, "test.csv")
+        self.invalid_train_file_path: str = os.path.join(self.invalid_data_dir, "train.csv")
+        self.invalid_test_file_path: str = os.path.join(self.invalid_data_dir, "test.csv")
+        self.drift_report_file_path: str = os.path.join(
+            self.data_validation_dir,
+            DATA_VALIDATION_DRIFT_REPORT_DIR,
+            DATA_VALIDATION_DRIFT_REPORT_FILE_NAME,
+        )
